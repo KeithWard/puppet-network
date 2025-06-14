802b2f2dcbb778bbeaf97e420f9c3d051f75b7bc
Keith Ward <keith@darkspace.me>
2025-06-14 23:35:17 +0100

    Add Type Aliases for AddressList

--- types/addressslist.pp
+++ types/addressslist.pp
@@ -1,3 +1,5 @@
-@summary Validate a list of IPV4 or IPV6 addresses
-type Network::AddressList = Variant[Stdlib::IP::Address::V4::Nosubnet, Array[Stdlib::IP::Address::V4::Nosubnet]]
-type Network::AddressList::CIDR = Variant[Stdlib::IP::Address::V4::CIDR, Array[Stdlib::IP::Address::V4::CIDR]]
+@summary Validate lists of IPV4 or IPV6 addresses
+type Network::AddressList::V4 = Variant[Stdlib::IP::Address::V4::Nosubnet, Array[Stdlib::IP::Address::V4::Nosubnet]]
+type Network::AddressList::V4::CIDR = Variant[Stdlib::IP::Address::V4::CIDR, Array[Stdlib::IP::Address::V4::CIDR]]
+type Network::AddressList::V6 = Variant[Stdlib::IP::Address::V6::Nosubnet, Array[Stdlib::IP::Address::V6::Nosubnet]]
+type Network::AddressList::V6::CIDR = Variant[Stdlib::IP::Address::V6::CIDR, Array[Stdlib::IP::Address::V6::CIDR]]
