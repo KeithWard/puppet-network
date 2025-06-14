3d1c0bc13a96698f49d91c502cf6d1b1bef04c7e
Keith Ward <keith@darkspace.me>
2025-06-14 23:57:26 +0100

    Add couple of extra tests
    Fix Typo

--- spec/unit/puppet/type/nm_connection_spec.rb
+++ spec/unit/puppet/type/nm_connection_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe 'the nm_connection type' do
   it 'loads' do
     expect(Puppet::Type.type(:nm_connection)).not_to be_nil
   end
+
+  it 'has a namevar' do
+    expect(Puppet::Type.type(:nm_connection).key_attributes).to eq(%i[name])
+  end
+
+  it 'has an ensure attribute' do
+    expect(Puppet::Type.type(:nm_connection).attrclass(:ensure)).not_to be_nil
+  end
 end
--- types/addressslist.pp
+++ types/addressslist.pp
@@ -1,4 +1,4 @@
-@summary Validate lists of IPV4 or IPV6 addresses
+# @summary Validate lists of IPV4 or IPV6 addresses
 type Network::AddressList::V4 = Variant[Stdlib::IP::Address::V4::Nosubnet, Array[Stdlib::IP::Address::V4::Nosubnet]]
 type Network::AddressList::V4::CIDR = Variant[Stdlib::IP::Address::V4::CIDR, Array[Stdlib::IP::Address::V4::CIDR]]
 type Network::AddressList::V6 = Variant[Stdlib::IP::Address::V6::Nosubnet, Array[Stdlib::IP::Address::V6::Nosubnet]]
